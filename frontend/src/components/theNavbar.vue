8d10c0638a3c2ce539ef22714e4b3a798169c16a
Syahdan basir <Syahdanbasir@students.amikom.ac.id>
2025-07-10 01:44:20 +0700

    perbaikan

---
+++ frontend/src/components/theNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="bg-white shadow-md sticky top-0 z-50 animate-fade-in">
+    <nav class="container mx-auto px-6 py-4 flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0">
+      <div class="text-2xl font-bold text-gray-800">Syahdan Basir</div>
+      <ul class="flex flex-col md:flex-row md:space-x-8 space-y-2 md:space-y-0">
+        <li>
+          <a href="#profil" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration-300">
+            Profil
+          </a>
+        </li>
+        <li>
+          <a href="#pendidikan" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration-300">
+            Pendidikan
+          </a>
+        </li>
+        <li>
+          <a href="#skill" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration-300">
+            Skill
+          </a>
+        </li>
+        <li>
+          <a href="#proyek" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration-300">
+            Proyek
+          </a>
+        </li>
+        <li>
+          <a href="#kontak" class="text-gray-600 hover:text-blue-500 transform hover:-translate-y-0.5 transition-all duration-300">
+            Kontak
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from '@/components/theNavbar.vue';
+import Hero from '@/components/theHero.vue';
+import Education from '@/components/theEducation.vue';
+import Skills from '@/components/theSkills.vue';
+import Projects from '@/components/theProjects.vue';
+import Contact from '@/components/theContact.vue';
+</script>
+<template>
+<div>
+<Navbar />
+<main>
+<Hero />
+<div v-animate-on-scroll><Education /></div>
+<div v-animate-on-scroll><Skills /></div>
+<div v-animate-on-scroll><Projects /></div>
+<div v-animate-on-scroll><Contact /></div>
+</main>
+</div>
+
+</template>
